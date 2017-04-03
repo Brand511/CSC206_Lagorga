b5005ce15d6521c041554be0be042ef98cbb1419
Brand511 <bllagorg@geneva.edu>
2017-04-03 01:44:34 -0400

    5.0 Deliverable

--- includes/application_includes.php
+++ includes/application_includes.php
@@ -3,3 +3,9 @@
 require_once('config.php');
 // Include the database connection and query class
 require_once('database.php');
+
+$db = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+$requestType = $_SERVER[ 'REQUEST_METHOD' ];
+
+session_start();
